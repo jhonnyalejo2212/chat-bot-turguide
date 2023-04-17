d36f3251a1b52d6f7306a344150e3920d3235e2a
jhonnyalejo2212 <jhonnyalejo2212@gmail.com>
2023-04-17 18:31:58 -0500

    Borrando sesiones de jhonny

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const JsonFileAdapter = require('@bot-whatsapp/database/json')
 
+/**
+ * Flows
+ */
 const flowPrincipal = require("./flows/flowPrincipal");
 
+/**
+ * Funcion principal
+ */
 const main = async () => {
   const adapterDB = new JsonFileAdapter()
   const adapterFlow = createFlow([flowPrincipal])
